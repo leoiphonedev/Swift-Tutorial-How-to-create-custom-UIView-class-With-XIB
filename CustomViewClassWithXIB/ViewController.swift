53baae8946c920a0fcf02c66a42b726ae95bfb67
Aman Aggarwal <aggarwalaman1985@gmail.com>
2020-03-29 15:06:50 +0530

    Initial Commit

---
+++ CustomViewClassWithXIB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomViewClassWithXIB
+//
+//  Created by Aman Aggarwal on 29/03/20.
+//  Copyright © 2020 Aman Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
